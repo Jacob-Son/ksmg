01190bdd23f1d34c847ad8c26574685b2b7ddbeb
Jacob-Son <shwwwwwww@naver.com>
2025-05-07 15:16:08 +0900

    fix: allow external access on port 8080

--- backend-main/src/main.ts
+++ backend-main/src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  // await app.listen(8080);
+  await app.listen(8080, '0.0.0.0'); // 외부 접속 허용
 }
 bootstrap();
