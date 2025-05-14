faa2b0c3fca728ee749afd6f7cb8214c3fbba6b3
Jacob-Son <shwwwwwww@naver.com>
2025-05-15 01:14:57 +0900

    fix front:tokenId

--- frontend-main/src/pages/store/[tokenId].tsx
+++ frontend-main/src/pages/store/[tokenId].tsx
@@ -61,26 +61,33 @@ export default function StorePage() {
   const [currentTab, setCurrentTab] = React.useState<string>('details');
   const tabs = isMobile ? mobileTabs : commonTabs;
 
-  // useEffect(() => {
-  //   if (nft === null) {
-  //     router.push('/');
-  //   }
-  // }, [nft]);
-
   useEffect(() => {
-    if (nft === null && !isNftPending) {
-      console.warn('🚨 NFT 데이터가 없음! 홈으로 이동:', nft);
+    if (nft === null) {
       router.push('/');
     }
-  }, [nft, isNftPending]);
+  }, [nft]);
 
   useEffect(() => {
-    console.log('NFT 데이터:', nft); // 👀 콘솔에서 NFT 데이터 확인
     if (!nft) return;
     setTotalLikeCount(nft?.totalLikeCount);
     setNftPrice(nft?.price);
     nftApi.addViewCount(collectionAddress, String(tokenId));
   }, [nft]);
+
+  // useEffect(() => {
+  //   if (nft === null && !isNftPending) {
+  //     console.warn('🚨 NFT 데이터가 없음! 홈으로 이동:', nft);
+  //     router.push('/');
+  //   }
+  // }, [nft, isNftPending]);
+
+  // useEffect(() => {
+  //   console.log('NFT 데이터:', nft); // 👀 콘솔에서 NFT 데이터 확인
+  //   if (!nft) return;
+  //   setTotalLikeCount(nft?.totalLikeCount);
+  //   setNftPrice(nft?.price);
+  //   nftApi.addViewCount(collectionAddress, String(tokenId));
+  // }, [nft]);
 
   const getType = () => {
     if (address && nft.ownerAddress === address) {
